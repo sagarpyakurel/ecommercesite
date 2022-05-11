8140910fb7634ce7449e20b4ffbdcec1d1dbfe30
sagarpyakurel <sagar.pkl01@gmail.com>
2022-05-11 10:53:32 +0545

    first commit upto django checkout but javascript axaj left

---
+++ ecommerce/store/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path("", views.store, name="store"),
+    path("cart/", views.cart, name="cart"),
+    path("checkout/", views.checkout, name="checkout"),
+    
+   
+]
